feceec04707f82cf8592d5345cc87498101fb378
Allyson Aberg <allya7897@gmail.com>
2017-03-21 21:31:36 -0400

    working?

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ if (!WIT_TOKEN) {
 }
 
 
-var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAAPCnjgJAvkBACZBidHZBZACz35igIGqULElTntgiAgVzIxVxAX7Y38Eb1N2FD2bKGZBT8OlL6a6F0qEJsWhYdWeZCdsbaZCTd13HT8QEwNPRa3owTNuAeqLG5HZAAzkOC1jiGhR90Nlb8mk3cylI0FsuZBf6lWWzMB5ZA3cXXVzgCQZDZD';
+var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAAPRFhieu38BAHjjDXZAVBR60sUrTYbrC5d5C3AZC2YHaJhYKWbLuVMk8USWh4bU5n7ZCap3Lmc8sBbSQeO28fGQChkl6ZC01InuErkZAfE2WmMqpYjj2RA3LJVDEpovAjqIrPOo42cGyywWYNJuWNZCaa6eXZCjn30Rstiw7sThQZDZD';
 if (!FB_PAGE_TOKEN) {
 	throw new Error('Missing FB_PAGE_TOKEN. Go to https://developers.facebook.com/docs/pages/access-tokens to get one.')
 }
