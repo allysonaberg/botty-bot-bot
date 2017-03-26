a88047701c03bb77e1ca49b2c9cd918dc7b349bd
Allyson Aberg <allya7897@gmail.com>
2017-03-26 10:12:20 -0400

    Commit

--- bot.js
+++ bot.js
@@ -9,6 +9,13 @@ var YouTube = require('youtube-node')
 var youTube = new YouTube()
 youTube.setKey('AIzaSyDxvDFk1sS41kxhWS8YR5etEGlHfkrExrI')
 
+//server
+var app = express()
+app.set('port', (process.env.PORT) || 5000)
+// SPIN UP SERVER
+app.listen(app.get('port'), function () {
+  console.log('Running on port', app.get('port'))
+})
 
 
 //WEATHER 
